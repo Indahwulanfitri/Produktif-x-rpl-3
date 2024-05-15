5e5ba328614014eb78f85f134ad442d38717331f
Indahwulanfitri <168519812+Indahwulanfitri@users.noreply.github.com>
2024-05-15 11:43:11 +0700

    Create Wulan_14_praktikum.js

---
+++ 30424/Wulan_14_praktikum.js
@@ -0,0 +1,143 @@
+const suhu = 30;
+if (suhu >16) {
+    console.log(" suhu lebih dari 16");
+} else {
+    console.log("suhu tidak lebih dari 16");
+  }
+  
+  const nilai = 100;
+  if (nilai >88) {
+  console.log("nilai lebih dari 88");
+ } else {
+     console.log("nilai tidak lebih dari 88");
+ }
+ 
+ const usia = 55
+ if (usia >= 46) { 
+ console.log("anda sudah tua.");
+ } else {
+   console.log("anda masih muda.");
+  }
+
+const berat_badan= 60;
+if (berat_badan <= 56) {
+console.log("berat tidak ideal.");
+} else {
+   console.log("berat ideal.");
+ }
+ 
+ const tinggi_badan =157;
+if (tinggi_badan >= 160) {
+console.log("tinggi_badan lebih dari 157");
+} else {
+   console.log("tinggi_badan tidak lebih dari 160");
+ }
+ 
+ var i= 5;
+ var withoutternnary;
+ 
+ 
+ 
+ if (i>10){
+      withoutternnary = "greater than 10";
+ } else {
+      withoutternnary = "less than 10"
+ }
+ 
+ const ternnaryanswer = i > 10 ? "greater than 10": "less than 10";
+ const ternnarywithif = i > 10 ? "greater than 10" : i < 5 ? "less than 5" :"between 5 and 10";
+console.log(withoutternnary)
+console.log(ternnaryanswer)
+console.log(ternnarywithif)
+
+
+var i= 9;
+ 
+ 
+ 
+ if (i>18){
+      withoutternary = "greater than 10";
+ } else {
+      withoutternary = "less than 10"
+ }
+ 
+ const ternaryanswer = i > 18 ? "greater than 18": "less than 18";
+ const ternarywithif = i > 18 ? "greater than 9" : i < 9 ? "less than 9" :"between 9 and 18";
+console.log(withoutternary)
+console.log(ternaryanswer)
+console.log(ternarywithif)
+
+var i= 11;
+ 
+ 
+ 
+ if (i>33){
+      withoutternaary = "greater than 10";
+ } else {
+      withoutternaary = "less than 10"
+ }
+ 
+ const ternaryaanswer = i > 33 ? "greater than 33": "less than 33";
+ const ternaarywithif = i > 33? "greater than 11" : i < 11 ? "less than 11" :"between 11 and 33";
+console.log(withoutternaary)
+console.log(ternaryaanswer)
+console.log(ternaarywithif)
+
+
+var i= 5;
+ var withoutternnary9;
+ 
+ 
+ 
+ if (i>15){
+      withoutternnary = "greater than 10";
+ } else {
+      withoutternnary = "less than 10"
+ }
+ 
+ const ternnaryanswer9 = i > 10 ? "greater than 15": "less than 10";
+ const ternnarywithif9 = i > 10 ? "greater than 15" : i < 5 ? "less than 5" :"between 5 and 15";
+console.log(withoutternnary9)
+console.log(ternnaryanswer9)
+console.log(ternnarywithif9)
+
+var i= 6;
+ var withoutternnary9;
+ 
+ 
+ 
+ if (i>17){
+      withoutternnary = "greater than 10";
+ } else {
+      withoutternnary = "less than 10"
+ }
+ 
+ const ternnaryanswer8 = i > 17 ? "greater than 17": "less than 16";
+ const ternnarywithif8 = i > 17 ? "greater than 17" : i < 5 ? "less than 6" :"between 6 and 17";
+console.log(withoutternnary9)
+console.log(ternnaryanswer9)
+console.log(ternnarywithif9)
+
+let x = 80;
+let y = 70;
+
+ //Contoh 1
+ let hasil1 = x > 67 ? "lebih besar dari 67" : "tidak mebih besar dari 67";
+ 
+ //contoh 2
+ let hasil2 = x === y ? "sama" : "tidak sama";
+ 
+ //contoh 3
+ let hasil3 = x % 3 === 0 ? "Genap" : "Ganjil";
+ 
+ //contoh 4
+ let hasil4 = x < y ? x : y;
+ 
+ //contoh5
+ let hasil5 = y > 160 ? "positif" : (y < 0) ?"Negatif" : "Nol";
+ 
+ console.log("Hasil 1:",hasil1);
+ console.log("Hasil 2:",hasil2);
+ console.log("Hasil 3:",hasil3);
+ console.log("Hasil 4:",hasil4);
+ console.log("Hasil 5:",hasil5);
